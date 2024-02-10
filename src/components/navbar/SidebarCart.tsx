87fda2e8094e11f038225ef6f2a556f763c16f7f
azedqsaze <sadm3979@gmail.com>
2024-02-10 11:16:51 +0000

    adjust navbar

--- src/components/navbar/SidebarCart.tsx
+++ src/components/navbar/SidebarCart.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ShoppingCart } from "lucide-react";
+import { ArrowRight, ShoppingCart } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 import {
@@ -15,9 +15,10 @@ import {
 import { Separator } from "../ui/separator";
 import { formatPrice } from "@/lib/utils";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 function SidebarCart() {
-  const productCount = 0;
+  const productCount: number = 0;
   const fee = 1;
   const total = 1;
 
@@ -28,37 +29,70 @@ function SidebarCart() {
       <SheetTrigger asChild>
         <ShoppingCart className="cursor-pointer" />
       </SheetTrigger>
-      <SheetContent className="w-full">
-        <SheetHeader>
-          <SheetTitle>Cart ({productCount})</SheetTitle>
-          <SheetDescription className="py-3">Cart Items</SheetDescription>
-        </SheetHeader>
-        <Separator />
-        <div className="flex flex-col  space-y-3 mt-4">
-          <div className="flex justify-between items-center ">
-            <h5 className=" tracking-wide">Shipping</h5>
-            <p className="font-semibold">Free</p>
+
+      {productCount === 0 ? (
+        // empty cart
+        <SheetContent className="w-full h-full flex flex-col justify-center items-center">
+          <SheetTitle className="text-center flex-grow-0 ">
+            Cart ({productCount})
+          </SheetTitle>
+          <div className="h-full w-full flex-grow flex flex-col justify-center items-center">
+            <Image
+              className=" object-contain"
+              alt="empty-cart"
+              src="/pngwing.com.png"
+              width={100}
+              height={100}
+            />
+            <SheetFooter>
+              <SheetClose asChild>
+                <Button
+                  variant={"link"}
+                  className="w-full mt-4 text-gray-700"
+                  onClick={() => router.push("/products")}
+                >
+                  Add your items to cart then checkout <ArrowRight width={20} />
+                </Button>
+              </SheetClose>
+            </SheetFooter>
           </div>
-          <div className="flex justify-between items-center ">
-            <h5 className=" tracking-wide">Transaction Fee</h5>
-            <p className="font-semibold">{formatPrice(fee)}</p>
+        </SheetContent>
+      ) : (
+        // not empty cart
+        <SheetContent className="w-full h-full">
+          <SheetHeader>
+            <SheetTitle className="text-center">
+              Cart ({productCount})
+            </SheetTitle>
+            <SheetDescription className="py-3">Cart Items</SheetDescription>
+          </SheetHeader>
+          <Separator />
+          <div className="flex flex-col  space-y-3 mt-4">
+            <div className="flex justify-between items-center ">
+              <h5 className=" tracking-wide">Shipping</h5>
+              <p className="font-semibold">Free</p>
+            </div>
+            <div className="flex justify-between items-center ">
+              <h5 className=" tracking-wide">Transaction Fee</h5>
+              <p className="font-semibold">{formatPrice(fee)}</p>
+            </div>
+            <div className="flex justify-between items-center ">
+              <h5 className=" tracking-wide">Total</h5>
+              <p className="font-semibold">{formatPrice(total)}</p>
+            </div>
           </div>
-          <div className="flex justify-between items-center ">
-            <h5 className=" tracking-wide">Total</h5>
-            <p className="font-semibold">{formatPrice(total)}</p>
-          </div>
-        </div>
-        <SheetFooter>
-          <SheetClose asChild>
-            <Button
-              className="w-full mt-4"
-              onClick={() => router.push("/cart")}
-            >
-              Go To Cart
-            </Button>
-          </SheetClose>
-        </SheetFooter>
-      </SheetContent>
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button
+                className="w-full mt-4"
+                onClick={() => router.push("/cart")}
+              >
+                Go To Cart
+              </Button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      )}
     </Sheet>
   );
 }
